6126443fc010dbe8efbac74ceaef33d63ec0a6ba
Zer000reZ <zerolelouchindex1@gmail.com>
2024-09-05 18:15:50 +0200

    removed ini switched to oeometa property

--- script/eos.outer.js
+++ script/eos.outer.js
@@ -1,4 +1,4 @@
-/* Milovana.com Resource File - Copyright 2006-2022 Milovana.com */
+/* Milovana.com Resource File - Copyright 2006-2022 Milovana.com !modified*/
 jQuery(function () {
   var iframe = document.getElementsByClassName('eosIframe')[0]
   var script
@@ -12,21 +12,26 @@ jQuery(function () {
       var key = document.body.getAttribute('data-key')
       var link = document.querySelector("link[rel~='icon']");
       if (!link) {
-          link = document.createElement('link');
-          link.rel = 'icon';
-          document.head.appendChild(link);
+        link = document.createElement('link');
+        link.rel = 'icon';
+        document.head.appendChild(link);
       }
       link.href = './icon.png?' + Date.now();
-      return fetch('./config.ini?' + Date.now()).then(x => x.text()).then(y => {
-        var config = parseINIString(y)
-        title = config.title
-        author = config.author
+      return fetch('eosscript.json?_dt=' + Date.now() + '&id=' + teaseId + (preview ? '&preview=1' : '') + (key ? '&key=' + key : ''), { credentials: 'same-origin' }).then(function (res) { return res.json() }).then(function (json) {
+        if (json.hasOwnProperty("oeosmeta")) {
+          if (json["oeosmeta"].hasOwnProperty("title")) {
+            title = json['oeosmeta']['title'];
+          }
+          if (json["oeosmeta"].hasOwnProperty("teaseId")) {
+            teaseId = json['oeosmeta']['teaseId'];
+          }
+          if (json["oeosmeta"].hasOwnProperty("author")) {
+            author = json['oeosmeta']['author'];
+          }
+        }
         document.title = title
-        preview = preview || config.preview === 'true'
-        return fetch('eosscript.json?_dt='+Date.now()+'&id=' + teaseId + (preview ? '&preview=1' : '') + (key ? '&key=' + key : ''), { credentials: 'same-origin' }).then(function (res) { return res.json() }).then(function (json) {
-          script = json
-          return { title: title, author: author, script: script, preview: preview }
-        })
+        script = json
+        return { title: title, author: author, script: script, preview: preview }
       })
     }, queryMedia: function (params) {
       var authorId = Number(document.body.getAttribute('data-author-id'))
@@ -79,8 +84,8 @@ jQuery(function () {
       dialog.style.display = 'flex'
       var messageEl = document.querySelector('.eosRatingDialog .message')
       messageEl.innerHTML = 'Thanks for playing!<br><br>'
-      +'<div style="font-size: 80%">You can '
-      +'<a style="color:white" href="#" onclick="window.location.reload()">reload</a> if you want.</div>'
+        + '<div style="font-size: 80%">You can '
+        + '<a style="color:white" href="#" onclick="window.location.reload()">reload</a> if you want.</div>'
       /*
       if (!isLoggedIn) {
         var messageEl = document.querySelector('.eosRatingDialog .message')
@@ -120,34 +125,5 @@ jQuery(function () {
       messageEl.innerHTML = 'Loading...'
       fetch('/graphql/', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ query: 'mutation { rateTease(teaseId: ' + teaseId + ', rating: ' + rating + ') { id } }' }) }).then(function (res) { return res.json() }).then(function (json) { if (json.data) { messageEl.innerHTML = 'Thanks for submitting your rating!' } else { messageEl.innerHTML = 'Sorry, an error occurred. Please try again later.' } })
     })
-  }
-  function parseINIString(data){
-    var regex = {
-        section: /^\s*\[\s*([^\]]*)\s*\]\s*$/,
-        param: /^\s*([^=]+?)\s*=\s*(.*?)\s*$/,
-        comment: /^\s*;.*$/
-    };
-    var value = {};
-    var lines = data.split(/[\r\n]+/);
-    var section = null;
-    lines.forEach(function(line){
-        if(regex.comment.test(line)){
-            return;
-        }else if(regex.param.test(line)){
-            var match = line.match(regex.param);
-            if(section){
-                value[section][match[1]] = match[2];
-            }else{
-                value[match[1]] = match[2];
-            }
-        }else if(regex.section.test(line)){
-            var match = line.match(regex.section);
-            value[match[1]] = {};
-            section = match[1];
-        }else if(line.length == 0 && section){
-            section = null;
-        };
-    });
-    return value;
   }
 })
